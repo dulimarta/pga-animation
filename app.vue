c0ed108c8b72669420ef7396ea1706ca6a52e19d
Hans Dulimarta <hans.dulimarta@gmail.com>
2023-09-08 23:46:42 -0400

    Setup ThreeJS

--- app.vue
+++ app.vue
@@ -1,20 +1,54 @@
 <template>
   <v-btn>Hans Here</v-btn>
   <v-icon>mdi-wifi-alert</v-icon>
-  
+  <canvas id="glcanvas" ref="glcanvas"></canvas>
+
   <!--div>
     <NuxtWelcome />
   </!--div-->
 </template>
 <script lang="ts" setup>
-import Algebra from 'ts-geometric-algebra';
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+const glcanvas: Ref<HTMLCanvasElement | null> = ref(null);
+const camera = new PerspectiveCamera(60, 4 / 3, 0.5, 500);
+// camera.updateProjectionMatrix()
+const scene = new Scene();
+camera.position.z = 100;
+let animationFrameHandle: number | null = null;
+let renderer: WebGLRenderer;
+onMounted(() => {
+  console.debug("Canvas at", glcanvas.value)
+  renderer = new WebGLRenderer({
+    canvas: glcanvas.value!,
+  });
+  renderer.setSize(450, 300)
+  
+  renderer.setClearColor(0xffff00, 1);
+  updateGraphics();
+});
 
-const Complex = Algebra(0, 1)
-const a = new Complex([3, 2])
-const b = new Complex([-3,5])
-console.log("Is this a complex number", a, b)
-console.log("I'm here")
+onBeforeUnmount(() => {
+  if (animationFrameHandle != null) cancelAnimationFrame(animationFrameHandle);
+});
+function updateGraphics() {
+  renderer.render(scene, camera);
+  animationFrameHandle = requestAnimationFrame(() => updateGraphics());
+}
+// import Algebra from 'ts-geometric-algebra';
+
+// const Complex = Algebra(0, 1)
+// const a = new Complex([3, 2])
+// const b = new Complex([-3,5])
+// console.log("Is this a complex number", a, b)
+// console.log("I'm here")
 </script>
 <style lang="scss">
-@use "./settings"
+@use "./settings";
+
+#glcanvas {
+  width: 450px;
+  height: 300px;
+  // background-color: red;
+  border: 2px solid red;
+}
 </style>
