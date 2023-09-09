4a225686aba7477cf48e7918f59e3082ef9c456a
Hans Dulimarta <hans.dulimarta@gmail.com>
2023-09-08 20:27:21 -0400

    First commit, add Vuetify, and ts-geometric-algebra

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn>Hans Here</v-btn>
+  <v-icon>mdi-wifi-alert</v-icon>
+  
+  <!--div>
+    <NuxtWelcome />
+  </!--div-->
+</template>
+<script lang="ts" setup>
+import Algebra from 'ts-geometric-algebra';
+
+const Complex = Algebra(0, 1)
+const a = new Complex([3, 2])
+const b = new Complex([-3,5])
+console.log("Is this a complex number", a, b)
+console.log("I'm here")
+</script>
+<style lang="scss">
+@use "./settings"
+</style>
