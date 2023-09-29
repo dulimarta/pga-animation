377534ef6fbd1251d39c4b53a6f5283390df798f
Hans Dulimarta <hans.dulimarta@gmail.com>
2023-09-28 21:38:59 -0400

    Add bike frame

--- components/ThreeCanvas.vue
+++ components/ThreeCanvas.vue
@@ -15,6 +15,7 @@ import {
   Group,
   CylinderGeometry,
   Matrix4,
+  MathUtils,
   AxesHelper,
   PlaneGeometry,
   TextureLoader,
@@ -25,39 +26,39 @@ import { storeToRefs } from "pinia";
 import { mul } from "ts-geometric-algebra";
 const glcanvas: Ref<HTMLCanvasElement | null> = ref(null);
 const PGAStore = usePGAStore();
-const TIRE_RADIUS = 13
-const TIRE_TUBE_RADIUS = 1.5
-const WHEEL_BASE = 48 // inches
+const TIRE_RADIUS = 13;
+const TIRE_TUBE_RADIUS = 1.5;
+const WHEEL_BASE = 48; // inches
 
-const WHEEL_RADIUS = TIRE_RADIUS + TIRE_TUBE_RADIUS
+const WHEEL_RADIUS = TIRE_RADIUS + TIRE_TUBE_RADIUS;
 
 let camera: PerspectiveCamera;
 const textureLoader = new TextureLoader();
 const scene = new Scene();
-scene.add(new AxesHelper(12));
+// scene.add(new AxesHelper(12));
 scene.add(new AmbientLight());
 // scene.background = new Color("skyblue");
 let animationFrameHandle: number | null = null;
 let { driveWheelSpeed, steerAngle } = storeToRefs(PGAStore);
 let speedFlipFactor = 1;
-let bodyRotation = 0
-let bodyXPosition = 0
-let bodyYPosition = 0
+let bodyRotation = 0;
+let bodyXPosition = 0;
+let bodyYPosition = 0;
 const driveAngularVelocity = computed(
   // 2 * PI * RotationPerMinute / 60 radians/second
   () => (Math.PI * driveWheelSpeed.value) / 30
 );
 const tanSteerAngle = computed(() =>
-  Math.tan(Math.PI * steerAngle.value/180)
-)
-let tireAngle = 0;
+  Math.tan((Math.PI * steerAngle.value) / 180)
+);
+let driveWheelAngle = 0;
 // let tirePosition = 0;
 let previousTimeStamp = 0;
 
 let renderer: WebGLRenderer;
-let bike: Group
-let driveWheel: Group
-let steeringWheel:Group
+let bike: Group;
+let driveWheel: Group;
+let steeringWheel: Group;
 onMounted(async () => {
   const marbleTexture = await textureLoader.loadAsync("marble.jpg");
   // console.debug("Texture", marbleTexture);
@@ -74,7 +75,7 @@ onMounted(async () => {
   const canvasHeight = glcanvas.value!.clientHeight;
   const canvasWidth = glcanvas.value!.clientWidth;
   camera = new PerspectiveCamera(45, canvasWidth / canvasHeight, 0.1, 1000);
-  camera.position.set(100, 100, 80);
+  camera.position.set(50, 100, 50);
   camera.up.set(0, 0, 1);
   camera.lookAt(0, 0, 0);
   bike = makeBike();
@@ -100,32 +101,40 @@ onBeforeUnmount(() => {
 
 function run_integrator(timeStamp: number /* in milliseconds */) {
   const elapsed = (timeStamp - previousTimeStamp) / 1000;
-  const bodyAngularVelocity = WHEEL_RADIUS / WHEEL_BASE * tanSteerAngle.value * driveAngularVelocity.value
-  const bodyLinearVelocity = WHEEL_RADIUS * driveAngularVelocity.value
-  bodyRotation += bodyAngularVelocity * elapsed
+  const bodyAngularVelocity =
+    (WHEEL_RADIUS / WHEEL_BASE) *
+    tanSteerAngle.value *
+    driveAngularVelocity.value;
+  const bodyLinearVelocity = WHEEL_RADIUS * driveAngularVelocity.value;
+  bodyRotation += bodyAngularVelocity * elapsed;
   if (Math.abs(bodyAngularVelocity) < 1e-6) {
-    bodyXPosition += Math.cos(bodyRotation) * elapsed * bodyLinearVelocity
-    bodyYPosition -= Math.sin(bodyRotation) * elapsed * bodyLinearVelocity
+    bodyXPosition += Math.cos(bodyRotation) * elapsed * bodyLinearVelocity;
+    bodyYPosition -= Math.sin(bodyRotation) * elapsed * bodyLinearVelocity;
   } else {
-    const multiplier = bodyLinearVelocity / bodyAngularVelocity
-    bodyXPosition += multiplier * (Math.sin(bodyRotation + elapsed * bodyAngularVelocity) - Math.sin(bodyRotation))
-    bodyYPosition -= multiplier * (-Math.cos(bodyRotation + elapsed * bodyAngularVelocity) + Math.cos(bodyRotation))
+    const multiplier = bodyLinearVelocity / bodyAngularVelocity;
+    bodyXPosition +=
+      multiplier *
+      (Math.sin(bodyRotation + elapsed * bodyAngularVelocity) -
+        Math.sin(bodyRotation));
+    bodyYPosition -=
+      multiplier *
+      (-Math.cos(bodyRotation + elapsed * bodyAngularVelocity) +
+        Math.cos(bodyRotation));
   }
-  tireAngle =
-    tireAngle - speedFlipFactor * driveAngularVelocity.value * elapsed;
-  if (Math.abs(bodyXPosition) > 500 || Math.abs(bodyYPosition) > 500) {
-    speedFlipFactor *= -1;
-  }
+  driveWheelAngle =
+    driveWheelAngle - speedFlipFactor * driveAngularVelocity.value * elapsed;
   previousTimeStamp = timeStamp;
 }
 function updateGraphics(timeStamp: number) {
   run_integrator(timeStamp);
-  driveWheel.rotation.z = tireAngle
-  steeringWheel.rotation.z = tireAngle
-  steeringWheel.rotation.y = -steerAngle.value * Math.PI/180
+  driveWheel.rotation.z = driveWheelAngle;
+
+  steeringWheel.rotation.z =
+    driveWheelAngle / Math.cos(MathUtils.degToRad(steerAngle.value));
+  steeringWheel.rotation.y = -MathUtils.degToRad(steerAngle.value);
   bike.position.x = bodyXPosition;
-  bike.position.y = bodyYPosition
-  bike.rotation.z = -bodyRotation
+  bike.position.y = bodyYPosition;
+  bike.rotation.z = -bodyRotation;
   renderer.render(scene, camera);
   animationFrameHandle = requestAnimationFrame((t) => updateGraphics(t));
 }
@@ -139,24 +148,58 @@ function updateGraphics(timeStamp: number) {
 
 function makeBike(): Group {
   const bikeFrame = new Group();
-  bikeFrame.add(new AxesHelper(24))
+  // bikeFrame.add(new AxesHelper(24))
   // X-positive is forward travel direction
   driveWheel = makeTire(TIRE_RADIUS, TIRE_TUBE_RADIUS);
-  driveWheel.position.x = -WHEEL_BASE/2
+  driveWheel.position.x = -WHEEL_BASE / 2;
   bikeFrame.add(driveWheel);
-  steeringWheel = makeTire(TIRE_RADIUS, TIRE_TUBE_RADIUS)
-  steeringWheel.translateX(WHEEL_BASE / 2)  
-  bikeFrame.add(steeringWheel)
-  bikeFrame.rotation.z = Math.PI/4
+  steeringWheel = makeTire(TIRE_RADIUS, TIRE_TUBE_RADIUS);
+  steeringWheel.translateX(WHEEL_BASE / 2);
+  bikeFrame.add(steeringWheel);
+  // bikeFrame.rotation.z = Math.PI / 4;
+  const seatTube = makePipe(24, 1, "red");
+  seatTube.rotateX(Math.PI / 2)
+  seatTube.translateY(12 + TIRE_RADIUS)
+  seatTube.rotateZ(MathUtils.degToRad(15))
+  seatTube.translateX(-8)
+  // seatTube.add(new AxesHelper(10));
+  bikeFrame.add(seatTube);
+  const downTube = makePipe(33, 1, 'red')
+  downTube.rotateX(MathUtils.degToRad(90))
+  downTube.translateY(9 + TIRE_RADIUS)
+  downTube.translateX(7)
+  downTube.rotateZ(MathUtils.degToRad(-45))
+  bikeFrame.add(downTube)
+  const topTube = makePipe(29, 1, 'red')
+  topTube.rotateZ(MathUtils.degToRad(90))
+  topTube.translateZ(33)
+  topTube.translateY(-4)
+  bikeFrame.add(topTube)
   // bikeFrame.position.x = 10
   return bikeFrame;
 }
+function makePipe(
+  pipeLength: number,
+  pipeRadius: number,
+  color?: string
+): Mesh {
+  const cylinderGeometry = new CylinderGeometry(
+    pipeRadius,
+    pipeRadius,
+    pipeLength
+  );
+  const cylinderMaterial = new MeshBasicMaterial({ color: color ?? "grey" });
+  const mesh = new Mesh(cylinderGeometry, cylinderMaterial);
+  // mesh.add(new AxesHelper(12))
+  return mesh
+}
+
 function makeTire(tireRadius: number, tubeRadius: number): Group {
-  const NUM_SPOKES = 6;
+  const NUM_SPOKES = 5;
   const tireGroup = new Group();
   tireGroup.translateZ(tubeRadius + tireRadius);
   tireGroup.rotateX(Math.PI / 2);
-  tireGroup.add(new AxesHelper(10))
+  // tireGroup.add(new AxesHelper(10))
   const torusGeometry = new TorusGeometry(tireRadius, tubeRadius, 10);
   const torusMaterial = new MeshBasicMaterial({ color: "black" });
   const tire = new Mesh(torusGeometry, torusMaterial);
@@ -164,13 +207,11 @@ function makeTire(tireRadius: number, tubeRadius: number): Group {
   const translation = new Matrix4().makeTranslation(0, tireRadius / 2, 0);
   const rotation = new Matrix4();
   for (let k = 0; k < NUM_SPOKES; k++) {
-    const cylinderGeometry = new CylinderGeometry(0.6 * tubeRadius, 0.6 * tubeRadius, tireRadius);
-    const cylinderMaterial = new MeshBasicMaterial({ color: "grey" });
-    const cylinder = new Mesh(cylinderGeometry, cylinderMaterial);
+    const spoke = makePipe(tireRadius, 0.6 * tubeRadius);
     rotation.makeRotationZ((k * 2 * Math.PI) / NUM_SPOKES);
-    cylinder.applyMatrix4(translation);
-    cylinder.applyMatrix4(rotation);
-    tireGroup.add(cylinder);
+    spoke.applyMatrix4(translation);
+    spoke.applyMatrix4(rotation);
+    tireGroup.add(spoke);
   }
   return tireGroup;
 }
