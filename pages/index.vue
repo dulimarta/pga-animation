c8325bd0d7a31bb15d1c3b4c12cff59e55069244
Hans Dulimarta <hans.dulimarta@gmail.com>
2023-11-15 11:37:48 -0500

    Rearrange input components in v-tabs

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,21 @@
     <div id="main">
       <ThreeCanvas />
       <div>
+      <v-tabs v-model="currentTab">
+        <v-tab>Run</v-tab>
+        <v-tab>Setup</v-tab>
+      </v-tabs>
+      <v-window v-model="currentTab">
+      <v-window-item>
         <InputControl />
-        <FloorMap/>
         <XYPlot :x-value="bodyPosition.x" :y-value="bodyPosition.y" title="XY Position"/>
+      </v-window-item>
+        <v-window-item>
+          <FloorMap/>
+        </v-window-item>
+      </v-window>
+    </div>
+      <div>
       </div>
     </div>
 </template>
@@ -13,6 +25,7 @@ import { storeToRefs } from "pinia";
 import { usePGAStore } from "~/store/pga-store";
 const store = usePGAStore();
 const { bodyPosition } = storeToRefs(store);
+const currentTab = ref(0)
 </script>
 <style scoped>
 #main {
